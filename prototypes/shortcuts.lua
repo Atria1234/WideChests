7993ed8a25c59d146c2a4f19a54d6dca789ff41b
ST-DDT <ST-DDT@gmx.de>
2025-01-17 13:06:29 +0100

    6.2.2

--- prototypes/shortcuts.lua
+++ prototypes/shortcuts.lua
@@ -9,6 +9,7 @@ data:extend(
 		icon = '__WideChests__/graphics/icons/merge-shortcut.png',
 		icon_size = 32,
 		small_icon = '__WideChests__/graphics/icons/merge-shortcut.png',
-		small_icon_size = 32
+		small_icon_size = 32,
+		associated_control_input = MergingChests.custom_input_names.merge_tool
 	}
 })
